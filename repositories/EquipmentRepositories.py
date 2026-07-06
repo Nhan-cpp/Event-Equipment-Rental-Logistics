70d9099295f107f544fe67f70676afb5872ac5bd
Nguyễn Văn Duy Nhân <nhannguyen120216@gmail.com>
2026-07-06 20:55:09 +0700

    Implement search and append functionality in EquipmentServices; enhance searchById in EquipmentRepositories

--- repositories/EquipmentRepositories.py
+++ repositories/EquipmentRepositories.py
@@ -3,7 +3,6 @@ from models.Equipment import Equipment
 class EquipmentRepositories():
     FILE_PATH = 'data/equipmentData.txt'
     __equipmentList = []
-
     def __init__(self):
         self.__equipmentList = self.loadEquipments()
     
@@ -11,16 +10,23 @@ class EquipmentRepositories():
         pass
     def saveEquipments(self):
         pass
-
-    def searchById(self):
-        pass
+    def searchById(self, equipmentID):
+        for index in range(len(self.__equipmentList)):
+            if self.__equipmentList[index].ID == equipmentID:
+                return index
+        return -1
     def searchByStatus(self):
         pass
-    def append(self):
-        pass
+    def append(self, new_equipment):
+        self.__equipmentList.append(new_equipment)
+        return True
     def update(self):
         pass
-    def sort(self):
-        pass
+    def sort(self, sortType, isReverse):
+        criteria = {
+            "hourlyRentalRate": lambda x: x.hourlyRentalRate,
+            "powerRating": lambda x: x.powerRating
+        }
+        return sorted(self.__equipmentList, key=criteria[sortType], reverse=isReverse)
     def groupByStatus(self):
         pass
--- services/EquipmentServices.py
+++ services/EquipmentServices.py
@@ -1,3 +1,4 @@
+from models.Equipment import Equipment
 from repositories.EquipmentRepositories import EquipmentRepositories
 
 class EquipmentServices():
@@ -5,15 +6,26 @@ class EquipmentServices():
     def __init__(self):
         self.__repositories = EquipmentRepositories()
     
-    def searchById(self):
-        pass
+    def searchById(self,equipmentID):
+        return self.__repositories.searchById(equipmentID)
+    
     def searchByStatus(self):
         pass
-    def append(self):
-        pass
+    def append(self, new_equipment : Equipment):
+        if self.__repositories.searchById(new_equipment.Id) != -1:
+            return False
+        if new_equipment.powerRating <= 0 or new_equipment.hourlyRentalRate <= 0:
+            return False 
+
+        return self.__repositories.append(new_equipment)
+
     def update(self):
         pass
-    def sort(self):
-        pass
+    def sort(self, sortType, isReverse):
+        if sortType != "hourlyRentalRate" and sortType != "powerRating":
+            return []  # Trả về danh sách rỗng nếu kiểu sort không hợp lệ
+
+        return self.__repositories.sort(sortType, isReverse)
+    
     def groupByStatus(self):
         pass
