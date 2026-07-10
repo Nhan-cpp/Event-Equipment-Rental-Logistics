ec6781cf5e0f8e20f2ed934eb86fd19321742ea2
Nguyễn Văn Duy Nhân <nhannguyen120216@gmail.com>
2026-07-10 14:38:15 +0700

    Refactor Rental class ID validation and error handling; improve clientName and expectedReturnTime setters for better input validation and exception management.

--- models/Rental.py
+++ models/Rental.py
@@ -5,7 +5,8 @@ class Rental():
     _clientName = None
     _startTime = None
     _expectedReturnTime = None
-
+    MIN_ID_LEN = 8
+    MAX_ID_LEN = 20
     def __init__(self,Id,clientName,startTime,expectedReturnTime):
         self._Id = Id
         self._clientName = clientName
@@ -16,19 +17,20 @@ class Rental():
     def Id(self):
         return self._Id
     @Id.setter
-    def Id(self,new_value):
-        if(validate_ID(new_value) == True):
-            self._Id = new_value
+    def Id(self,new_value : str):
+        if(len(new_value) < self.MIN_ID_LEN and self.MAX_ID_LEN < len(new_value)):
+            raise ValueError(f"Rental id need length from {self.MIN_ID_LEN} to {self.MAX_ID_LEN}")
+        for character in new_value:
+            if not character.isalnum():
+                raise ValueError("Rental Id only contain character or digit.")
+        self._Id = new_value
     
     @property
     def clientName(self):
         return self._clientName
     @clientName.setter
-    def clientName(self,new_value):
-        try:
-            self._clientName = str(new_value)
-        except:
-            raise ValueError("client name have some error.")
+    def clientName(self,new_value : str):
+        self._clientName = str(new_value)
     @property
     def startTime(self):
         return self._startTime
@@ -44,7 +46,8 @@ class Rental():
     @expectedReturnTime.setter
     def expectedReturnTime(self,new_value):
         try:
-            if(self._startTime >= new_value):
-                self._expectedReturnTime = new_value
+            if(self._startTime < new_value):
+                raise ValueError("Rental expected return time must early than start time")
+            self._expectedReturnTime = new_value
         except:
-            raise ValueError("expected return time have some error.")        
+            raise ValueError("Rental expected return time have some error.")        
